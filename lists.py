5567e32ac2f605bf150a3ae64a13d6df625748b0
Sierzko <adagioo1993@gmail.com>
2021-11-02 19:08:39 +0100

    daily commit

---
+++ lists.py
@@ -0,0 +1,20 @@
+numbers = [2, 2, 4, 1, 1, 3, 5]
+
+numbers.remove(1)
+numbers.extend([0])
+print(numbers)
+numbers.append(len(numbers))
+print(numbers)
+numbers.remove(5)
+numbers.append(5)
+
+tuple("")
+
+# (,)
+
+# # ()
+
+print(type(()))
+
+
+# ([])
